9388a82522a9465ef9969a9ede23424e6501d372
nusgnojkrap <63632963+nusgnojkrap@users.noreply.github.com>
2022-01-19 09:59:09 +0900

    Add files via upload

---
+++ jongston/stack.js
@@ -0,0 +1,42 @@
+const { jongston } = require("./jongston");
+
+class LoggerStack {
+    constructor(filepath, filename) {
+        this.logger = [];
+        this.start = false;
+        this.currentLogger = null;
+
+        this.filepath = filepath;
+        this.filename = filename;
+    }
+
+    pop() {
+        this.currentLogger = this.logger.pop();
+    }
+
+    process() {
+        return new Promise(async (resolve) => {
+            while (true) {
+                if (this.logger.length === 0) {
+                    this.start = false;
+                    return resolve();
+                } else {
+                    this.pop();
+                    await jongston(this.filepath, this.filename, this.currentLogger);
+                }
+            }
+        });
+    }
+
+    insert(logger) {
+        this.logger.push(logger);
+
+        if (this.start) {
+        } else {
+            this.start = true;
+            this.process();
+        }
+    }
+}
+
+module.exports.LoggerStack = LoggerStack;
