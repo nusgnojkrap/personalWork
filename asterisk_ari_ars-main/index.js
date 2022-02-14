25fa9ecbb5a4287402e2dbbbce70d196e3f3370c
Jong Sun <ahrl1994@gmail.com>
2022-02-14 17:18:53 +0900

    Revert "Concurrent dev"
    This reverts commit 1c35c2a59d423d1390b5d9cf043f161de50c64d5.

--- asterisk_ari_ars-main/index.js
+++ asterisk_ari_ars-main/index.js
@@ -7,7 +7,6 @@ var jsName = path.basename(__filename);
 
 const ariService = require("./services/ariService");
 const { stringify } = require("querystring");
-const { concurrentQueue, MyQueueClass } = require("./utils/limiter");
 
 const app = express();
 
@@ -24,30 +23,14 @@ app.use(bodyParser.json());
 //발신자번호    callerId
 
 app.post("/", function (req, res) {
-    let myQueue = new MyQueueClass();
-    //동기로 자신의 active 실행 -> 끝날 떄 까지 기다려짐 왜냐면 동기닌깐
-    //기다리다가 끝나면 return 해당 유저에게
+    logger.info(jsName, "start index.js");
+    logger.info(jsName, "req.body : " + JSON.stringify(req.body));
 
-    async function myFunc(callback) {
-        await mySetTimeout();
-        callback();
-        return "end";
+    if (ariService(req.body) == "success") {
+        res.send("success");
+    } else {
+        res.send("fail");
     }
-
-    function mySetTimeout() {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve();
-            }, 10000);
-        });
-    }
-
-    let a = myFunc.bind(null, function () {
-        console.log("end");
-    });
-
-    myQueue.concurrentQueue.push(a);
-    myQueue.queue2SCV();
 });
 
 app.listen(3000);
