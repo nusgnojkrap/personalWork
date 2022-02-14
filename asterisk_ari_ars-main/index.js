1c35c2a59d423d1390b5d9cf043f161de50c64d5
unknown <api@sejongtelecom.net>
2022-02-14 17:06:24 +0900

    Concurrent dev

--- asterisk_ari_ars-main/index.js
+++ asterisk_ari_ars-main/index.js
@@ -7,6 +7,7 @@ var jsName = path.basename(__filename);
 
 const ariService = require("./services/ariService");
 const { stringify } = require("querystring");
+const { concurrentQueue, MyQueueClass } = require("./utils/limiter");
 
 const app = express();
 
@@ -23,14 +24,30 @@ app.use(bodyParser.json());
 //발신자번호    callerId
 
 app.post("/", function (req, res) {
-    logger.info(jsName, "start index.js");
-    logger.info(jsName, "req.body : " + JSON.stringify(req.body));
+    let myQueue = new MyQueueClass();
+    //동기로 자신의 active 실행 -> 끝날 떄 까지 기다려짐 왜냐면 동기닌깐
+    //기다리다가 끝나면 return 해당 유저에게
 
-    if (ariService(req.body) == "success") {
-        res.send("success");
-    } else {
-        res.send("fail");
+    async function myFunc(callback) {
+        await mySetTimeout();
+        callback();
+        return "end";
     }
+
+    function mySetTimeout() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, 10000);
+        });
+    }
+
+    let a = myFunc.bind(null, function () {
+        console.log("end");
+    });
+
+    myQueue.concurrentQueue.push(a);
+    myQueue.queue2SCV();
 });
 
 app.listen(3000);
